a6b6c8d31454adb0d840270c6d43cd616e19e093
aungksan4-coder <aung.k.san4@gmail.com>
2026-08-25 14:27:39 +0630

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -934,7 +934,7 @@ elif selected_page == "MSOps6 & FiberOps6 Box Data":
                             st.session_state.box_gallery_overrides[card_key]["img1"] = None
                             save_box_state()
                             st.rerun()
-else:
+    else:
         up_img1 = st.file_uploader("Upload Left Photo", type=["png", "jpg", "jpeg"], key=f"up1_box_{card_key}", label_visibility="collapsed")
         if up_img1 is not None:
             cloud_url = upload_to_imgbb(up_img1)
